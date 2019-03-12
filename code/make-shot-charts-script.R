e9f55bd5a8573f74c673e2558c8af4d02bb17c6e
Dev Navani <devnavani@gmail.com>
2019-03-11 21:47:26 -0700

    second commit

--- code/make-shot-charts-script.R
+++ code/make-shot-charts-script.R
@@ -6,3 +6,17 @@ durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
 klay <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
 library(ggplot2)
 klay_scatterplot <- ggplot(data = klay) + geom_point(aes(x = x, y = y, color = shot_made_flag))
+court_file <- "../images/nba-court.jpg"
+library(jpeg)
+library(grid)
+court_image <- rasterGrob(
+  readJPEG(court_file),
+  width = unit(1, "npc"),
+  height = unit(1, "npc")
+)
+klay_shot_chart <- ggplot(data = klay) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Klay Thompson (2016 season)') +
+  theme_minimal()
